58b6a2964da3c660fa46d58f99dfc88653fc675d
hellby01 <aramdel2002@hotmail.com>
2023-04-27 21:43:24 +0200

    Tarea5ARD2.0

--- src/Tarea5Entornos/Tarea5Entornos.java
+++ src/Tarea5Entornos/Tarea5Entornos.java
@@ -3,5 +3,6 @@ package Tarea5Entornos;
 public class Tarea5Entornos {
 	public static void main (String args[]) {
 		System.out.println("Hola mundo!!" + "\n" + "Esta es mi tarea 5 de Etornos");
+		System.out.println("Correeccion practica 5");
 	}
 }
